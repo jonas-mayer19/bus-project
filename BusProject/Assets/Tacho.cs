a9c85dd06f38bcae74a35e5697ff4499b294798e
Percy233 <64072893+Percy233@users.noreply.github.com>
2020-10-12 23:58:46 +0200

    Added speedometer
    Currently combines and display velocity in all 3 axis. Once the vehicle controller is implemented, this can be changed to only display speed on the XZ plane.

---
+++ BusProject/Assets/Tacho.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tacho : MonoBehaviour
+{
+    public GameObject templateLabel;
+    public GameObject targetObject;
+    public int divisions;
+    public int maxLabel;
+    private Transform needle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLables();
+        needle = gameObject.transform.Find("Needle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = targetObject.GetComponent<Rigidbody>().velocity.magnitude;
+        needle.rotation = Quaternion.Euler(0, 0, CalcAngle(speed));
+    }
+
+    public void GenerateLables()
+    {
+        for (int i = 0; i <= maxLabel; i += (maxLabel / divisions))
+        {
+            GameObject lable = Instantiate(templateLabel, Vector3.zero, Quaternion.Euler(0, 0, CalcAngle(i)));
+            lable.transform.SetParent(gameObject.transform,false);
+            Transform number = lable.transform.Find("Number");
+            number.GetComponent<Text>().text = i.ToString();
+            number.eulerAngles = Vector3.zero;
+            lable.SetActive(true);
+        }
+    }
+    public float CalcAngle(float speed) {
+        return (45 - (speed * 270 / maxLabel));
+    }
+    }
