893d9eda6a20d05fb2c5a5b1d47b51f770451948
CoffeeCat0815 <72126659+CoffeeCat0815@users.noreply.github.com>
2020-10-14 18:41:38 +0200

    Pfeiltasten und Reifenbewegung
    Pfeiltasten und Reifenbewegung

---
+++ BusProject/Assets/Drive.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    
+    private Vector3 [] direction = new Vector3[2];
+    public float velocity = 3f;
+
+    
+
+    private void Move()
+    {
+        if(Input.GetKey("up"))
+        {
+            Rotation(true);                                                                  
+            this.transform.Translate(direction[0].normalized * velocity * Time.deltaTime );      //Translation zur Objektverschiebung auf Basis der Objektkoordinaten
+        }
+        else if (Input.GetKey("down"))
+        {
+            Rotation(false);
+            this.transform.Translate(direction[1].normalized * velocity * Time.deltaTime );
+        }      
+    }
+    private void Rotation(bool r)                                                               //boolean zur Unterscheidung von Vor- und Rückwärtsfahren
+    {
+        float a = -1 * velocity * 2;
+        if (Input.GetKey("right"))
+        {
+            if (r)                                                                              //pos y Kordinaten bei vorwärts, sonst negative
+            {
+                a *= -1;
+            }
+            this.transform.Rotate(0, a * velocity * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey("left"))
+        {
+            if (!r)                                                                             //neg y Kordinaten bei vorwärts, sonst positive
+            {
+                a *= -1;
+            }
+            this.transform.Rotate(0, a * velocity * Time.deltaTime, 0);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction[0] = new Vector3(0, 0, -1);                                                   //forward
+        direction[1] = new Vector3(0, 0, 1);                                                    //back
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+       
+    }
+    
+}
---
+++ BusProject/Assets/MovingWheels.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class MovingWheels : MonoBehaviour
+{
+    private float turn = 0f;
+    private GameObject bus;
+
+    void TurnWheels()
+    {
+        
+        
+        float ybus = bus.transform.eulerAngles.y;                                                        //zum zurücksetzten der Räder auf Busposition
+        var forwardBus = bus.transform.forward;                                                          //Winkelberechung zwischen Rad und Bus
+        var forwardWheel = transform.forward;
+        var angle = Vector3.Angle(forwardBus, forwardWheel);
+        Debug.Log( angle);
+       
+        if (Input.GetKey("right") && angle < 35)
+        {
+            turn += 0.2f;
+        }
+        else if (Input.GetKey("left") && angle < 35)
+        {
+            turn -= 0.2f;
+        }
+        else if (Input.GetKey("up") && !(Input.GetKey("left")|| Input.GetKey("right")))
+        {
+
+            turn = ybus;
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, turn, transform.eulerAngles.z);    //y-Rotation der Räder     
+
+            
+        }
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, turn, transform.eulerAngles.z);    //y-Rotation der Räder     
+     
+
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bus = GameObject.Find("Bus");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            TurnWheels();
+    }
+}
