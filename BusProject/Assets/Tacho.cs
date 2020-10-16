7934990371763339b26cfd1bab1475affdf6cab9
Percy233 <64072893+Percy233@users.noreply.github.com>
2020-10-16 10:25:21 +0200

    Add environment using Simple Town Assets

--- BusProject/Assets/Tacho.cs
+++ BusProject/Assets/Tacho.cs
@@ -9,20 +9,27 @@ public class Tacho : MonoBehaviour
     public int divisions;
     public int maxLabel;
     private Transform needle;
+    private Vector3 lastPos;
+    private Vector3 currPos;
     // Start is called before the first frame update
     void Start()
     {
+        ShowTacho(true);
         GenerateLables();
         needle = gameObject.transform.Find("Needle");
+        lastPos = targetObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 velocity = targetObject.GetComponent<Rigidbody>().velocity;
-        //float speed = velocity.magnitude; //In all axis
-        float speed = (new Vector3(velocity.x, 0, velocity.z)).magnitude; // Only in XZ plane
+        //Vector3 velocity = targetObject.GetComponent<Rigidbody>().velocity; //using rigidbody
+        //float speed = (new Vector3(velocity.x, 0, velocity.z)).magnitude; // Only in XZ plane using rigidbody
+        currPos = targetObject.transform.position;
+        Vector3 travel = (currPos - lastPos); //using travel over time        
+        float speed = (new Vector3(travel.x, 0, travel.z)).magnitude / Time.deltaTime; // Only in XZ plane using travel over time
         needle.rotation = Quaternion.Euler(0, 0, CalcAngle(speed));
+        lastPos = currPos;
     }
 
     public void GenerateLables()
@@ -41,4 +48,10 @@ public class Tacho : MonoBehaviour
     public float CalcAngle(float speed) {
         return (45 - (speed * 270 / maxLabel));
     }
+    public void ShowTacho(bool active)
+    {
+        gameObject.transform.Find("Dial").gameObject.SetActive(active);
+        gameObject.transform.Find("Needle").gameObject.SetActive(active);
+        gameObject.transform.Find("Description").gameObject.SetActive(active);
     }
+}
