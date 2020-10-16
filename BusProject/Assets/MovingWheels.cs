65680bfb4e2946616fc35475a40acfb7148bf0c6
CoffeeCat0815 <72126659+CoffeeCat0815@users.noreply.github.com>
2020-10-16 17:07:25 +0200

    wheels
    fixing code based wheels that get stuck and some of the rotation problems

--- BusProject/Assets/MovingWheels.cs
+++ BusProject/Assets/MovingWheels.cs
@@ -10,34 +10,51 @@ public class MovingWheels : MonoBehaviour
 
     void TurnWheels()
     {
-        
-        
+
+
         float ybus = bus.transform.eulerAngles.y;                                                        //zum zurücksetzten der Räder auf Busposition
         var forwardBus = bus.transform.forward;                                                          //Winkelberechung zwischen Rad und Bus
         var forwardWheel = transform.forward;
-        var angle = Vector3.Angle(forwardBus, forwardWheel);
-        Debug.Log( angle);
-       
-        if (Input.GetKey("right") && angle < 35)
+        var angle = Vector3.SignedAngle(forwardWheel, forwardBus, bus.transform.up);                     //angle in von -180 bis 180 grad
+        Debug.Log(angle);
+
+        if (Input.GetKey("right") && angle > -30)
         {
             turn += 0.2f;
         }
-        else if (Input.GetKey("left") && angle < 35)
+        else if (Input.GetKey("left") && angle < 30)
         {
             turn -= 0.2f;
         }
-        else if (Input.GetKey("up") && !(Input.GetKey("left")|| Input.GetKey("right")))
+        else if (Input.GetKey("up") && !(Input.GetKey("left") || Input.GetKey("right")))
         {
 
-            turn = ybus;
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, turn, transform.eulerAngles.z);    //y-Rotation der Räder     
+            if (turn < ybus)                                                                            //langsames zurückdrehen der Räder
+            {
+                turn += 0.1f;
+            }
+            else if (turn > ybus)
+            {
+                turn -= 0.1f;
+            }
 
-            
+
+        }
+        else if (Input.GetKey("down") && !(Input.GetKey("left") || Input.GetKey("right")))
+        {
+
+            if (turn < ybus)                                                                            //langsames zurückdrehen der Räder
+            {
+                turn += 0.2f;
+            }
+            else if (turn > ybus)
+            {
+                turn -= 0.2f;
+            }
+
         }
 
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, turn, transform.eulerAngles.z);    //y-Rotation der Räder     
-     
-
 
     }
 
